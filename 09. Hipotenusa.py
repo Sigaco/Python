fd88750a9fa65b3d331a769b17e09af590cc9ba9
Sigaco <141557119+Sigaco@users.noreply.github.com>
2025-02-06 12:46:01 +0100

    Add files via upload

---
+++ 09. Hipotenusa.py
@@ -0,0 +1,9 @@
+# Calcular la hipotenusa de un triángulo recto 💖
+
+print('Vamos a calcular la hipotenusa de una triángulo recto')
+
+a = int(input('Ingresa un número de 0 a 9 para el lado A: '))
+b = int(input('Ingresa otro número para el lado B: '))
+
+hi = (a ** 2)+(b ** 2) ** 0,5
+print(hi)
