40a5d3e95c9d4054261b572464dbccef6bcf2c20
Sigaco <141557119+Sigaco@users.noreply.github.com>
2025-02-06 12:46:24 +0100

    Add files via upload

---
+++ 10. divisas.py
@@ -0,0 +1,15 @@
+# Divisas #codedex 💖
+
+pesos = 20.60
+soles = 3.71
+reales = 5.80
+dolares = 1
+
+print('Vamos a calcular cuantos Euros tenemos después de un viaje por México, Perú y Brasil')
+p = int(input('Cuantos Pesos tienes?: '))
+s = int(input('Cuantos Soles tienes?: '))
+r = int(input('Cuantos Reales tienes?: '))
+
+total = (p * 20.60)+(s * 3.71)+(r * 5.80)
+
+print(total)
