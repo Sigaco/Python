de4255d31d391435f2abaa11fec1762a597af700
Sigaco <141557119+Sigaco@users.noreply.github.com>
2025-02-07 09:25:06 +0100

    Add files via upload
    Detección de Errores

---
+++ 11. Bugs.py
@@ -0,0 +1,11 @@
+butterflies = 10
+beetles = 12
+ladybugs = 20
+
+print('I caught ' + str(10) +' 🦋 butterflies!')
+print('I caught ' + str(12) + '🎆 beetles!')
+print('I caught ' + str(20) + ' 🐞 ladybugs!')
+
+total = butterflies + beetles + ladybugs
+
+print('I caught ' + str(total) + ' total bugs! 🦋🎆🐞 ')
