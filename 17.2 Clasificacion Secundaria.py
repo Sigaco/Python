677651cea6d83ab0ba531120c1fa4615ef127a66
Sigaco <141557119+Sigaco@users.noreply.github.com>
2025-02-11 09:20:41 +0100

    Add files via upload

---
+++ 17.2 Clasificacion Secundaria.py
@@ -0,0 +1,14 @@
+# Clasificación en secundaria #codedex
+
+grado = int(input('Indica cuantos años tiene el alumno: (Entre 9 y 12 años):  '))
+
+if grado == 9:
+    print('Estás en el grado Freshman ')
+elif grado == 10:
+    print('Estás en el grado Sophomore ')
+elif grado == 11:
+    print('Estás en el grado Junior ')
+elif grado == 12:
+    print('Estás en el grado Senior ')
+else:
+    print('No estás en la secundaria ')
