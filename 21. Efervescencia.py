f3357606278e664c46de31b31427055115f7a4ff
Sigaco <141557119+Sigaco@users.noreply.github.com>
2025-02-12 10:40:54 +0100

    Add files via upload

---
+++ 21. Efervescencia.py
@@ -0,0 +1,12 @@
+# Efervecescencia  #codedex ❤
+
+for i in range(101):
+    
+    if i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    elif i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    else:
+        print(i)
