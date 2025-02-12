90eddb6f4811a393b060def02e6e642c4246e69f
Sigaco <141557119+Sigaco@users.noreply.github.com>
2025-02-12 10:00:48 +0100

    Add files via upload

---
+++ 20. Botella de 20_99.py
@@ -0,0 +1,7 @@
+# Botella de 20,99  #codedex ❤
+
+for x in range(99, 0, -1):
+  print(str(x ) + str( ' Botellas de cerveza en la pared'))
+  print(str(x ) + str( ' Botellas de cerveza'))
+  print('Toma una y pasala')
+  print(str(x - 1) + str(' Botellas de cerveza en la pared'))
