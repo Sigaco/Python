2242c983c270dfe1d5523cc806b859cc11ae612b
Sigaco <141557119+Sigaco@users.noreply.github.com>
2025-02-13 09:04:15 +0100

    Add files via upload

---
+++ 21.6 Suma de cuadrados.py
@@ -0,0 +1,10 @@
+# Suma de cuadrados  #codedes ❤
+
+num = int(input("Ingrese un número entre 1 y 10:  "))
+
+cuadrado = num ** 2
+total = 0
+
+total = total + cuadrado
+
+print("El cuadrado de", num, "es", cuadrado)
